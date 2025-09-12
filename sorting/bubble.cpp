abc9603abc3bfbe18ca5e4b0eb45e09d9bb58253
Aman Kumar <amandsvv4006@gmail.com>
2025-09-13 00:32:09 +0530

    sligh changement doone in sorting algorithm

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -9,8 +9,8 @@ int main(){
     vector<int> nums = {1,4,2,5,2,6,8,7,0};
     int n = nums.size();
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n-i; j++){
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
             if(nums[j] > nums[j+1]){
                 int temp = nums[j];
                 nums[j] = nums[j+1];
