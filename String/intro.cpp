4ff79f58b25e78d4cc26f4afb59901476d975b70
Aman Kumar <amandsvv4006@gmail.com>
2025-09-25 22:06:12 +0530

    Deep diving i strings

--- String/intro.cpp
+++ String/intro.cpp
@@ -1,11 +1,46 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    string s = "Hii my name is Aman"; //advNC VERSION OF CHARACTER ARRAYS AS STRINGS
-    char str[20] = {'a','m','a','n'}; // character array
+bool wordBreak(string s, vector<string> &wordDict)
+{
+    int i = 0, j = 0;
+    bool isPossible = true;
+    string word = "";
+    while (j < s.length())
+    {
+        word += s[j++];
 
-    s.find("ab"); //function to find a substring in a string;
-    s.erase(1,2); //erases a part of string within the strt and end values given 
+        cout<<"Word is : "<<word<<endl;
+
+        for (string str : wordDict)
+        {
+            if (word == str)
+            {
+                i = j;
+                word = "";
+                isPossible = true;
+                break;
+            }
+            else
+            {
+                isPossible = false;
+            }
+        }
+        if(isPossible){
+            cout<<"true"<<endl;
+        }else{
+            cout<<"False"<<endl;
+        }
+    }
+    return isPossible;
+}
+
+int main()
+{
+    string s = "aaaaaaa";
+    vector<string> wordDict = {"aaaa", "aaa"};
+
+    cout<<wordBreak(s,wordDict)<<endl;
 }
