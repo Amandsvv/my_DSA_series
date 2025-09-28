0bd93083b0e98c7a8b1b3ca2f4573e87c74d1473
Aman Kumar <amandsvv4006@gmail.com>
2025-09-28 15:52:57 +0530

    Count of prime in range finding by seive of Eratosthenes

--- Math/prime.cpp
+++ Math/prime.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
@@ -12,9 +13,28 @@ string isPrime(int n){
     return "Prime";
 }
 
+//finding all prime in given n using seive of eratosthenes
+int count(int n){
+    int ans =0;
+    vector<bool> isPrime(n+1, true);
+
+    for(int i = 2; i < n ; i++){
+        if(isPrime[i]){
+            ans++;
+            for(int j = i*2; j < n ; j+=i){
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    return ans;
+}
+
 int main(){
     int n = 47;
 
     cout<< isPrime(n)<<endl;
+    cout<<"No. of Prime within the Given number is : "<<count(n)<<endl;
+    
     return 0;
 }
