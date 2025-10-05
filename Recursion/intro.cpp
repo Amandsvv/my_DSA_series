a7d80e56b812e1d2db5f0191d5c6035327b27f83
Aman Kumar <amandsvv4006@gmail.com>
2025-10-05 22:37:38 +0530

    print 1 to n by senfing n as paramter

--- Recursion/intro.cpp
+++ Recursion/intro.cpp
@@ -7,7 +7,16 @@ int sumN(int n){ // recursive function
     return n + sumN(n-1); // calling recusrive funnction again and again until n = 1 
 }
 
+void print(int n){
+    if(n < 1){
+        return;
+    }
+    print(n-1);
+    cout << n << " "; 
+}
+
 int main(){
     cout<<"Sum = "<<sumN(5)<<endl; // In main calling the recursive function
+    print(5);
     return 0;
 }
