b4c243408df326ca9f42b388be22f4b8463772ba
Aman Kumar <amandsvv4006@gmail.com>
2025-12-03 23:53:24 +0530

    revised bubble sort

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -6,9 +6,9 @@ using namespace std;
 void bubbleSort(int arr[], int n){
     for(int i = 0; i< n-1; i++){
         bool isSwaped = false;
-        for(int j = 0; j < n-i-1; j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+        for(int j = 0; j < n-i-1; j++){ // j -> n-i-1
+            if(arr[j]>arr[j+1]){ // curr is greator -> than next one
+                swap(arr[j],arr[j+1]); // swap and make it as curr
                 isSwaped = true;
             }
         }
